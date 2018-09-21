3b960533c1a9ba9e74015a21aab22ee4587d9178
JackWong992 <jackwong992@163.com>
2018-09-21 21:57:48 +0800

    updated

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # vuecli-fir
 
+## 项目目录搭建
+1. 具体讲解请看： [目录搭建](https://www.yuque.com/feelone/vuesz/issue-1)
+
 ## Project setup
 ```
 npm install
--- vue.config.js
+++ vue.config.js
@@ -1,37 +1,17 @@
-// const path = require('path')  //引入node模块
-// const resolve = (dir)=>path.join(__dirname, dir)
-
-// const BASE_URL = process.env.NODE_ENV === 'procution'?'/iview-admin':'/'
-// //判断当前环境是开发环境还是真实环境
-// module.exports = {
-//   lintOnSave: false,
-//   baseUrl: BASE_URL,
-//   chainWebpack: config =>{
-//     config.resolve.alias
-//       .set('@',resolve('src'))  //@代表src路径
-//       .set('_c', resolve('src/components'))
-//   },
-//   procutionSourceMap:false,
-//   //打包的时候不生成Map文件，加快打包设置
-//   devServer: {
-//     proxy: "http://localhost:4000"  //代理的URL，开发服务器请求静态资源引向这个地址。
-//   }
-// }
-
 const path = require('path')
 const resolve = dir => path.join(__dirname, dir)
 
 const BASE_URL = process.env.NODE_ENV === 'procution' ? '/iview-admin/' : '/'
-
+ //判断当前环境是开发环境还是真实环境
 module.exports = {
   lintOnSave: false,
   baseUrl: BASE_URL,
   chainWebpack: config => {
     config.resolve.alias
-      .set('@', resolve('src'))
+      .set('@', resolve('src'))//@代表src路径
       .set('_c', resolve('src/components'))
   },
-  // 打包时不生成.map文件
+  // //打包的时候不生成Map文件，加快打包设置
   productionSourceMap: false,
   devServer: {
     proxy: 'http://localhost:4000'
